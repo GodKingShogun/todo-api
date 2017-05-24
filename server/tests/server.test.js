f1c3f628ae997bd30498add788f0c46a073a4e90
David Amazing <David Amazing>
2017-05-24 15:56:13 -0400

    added test for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,16 +4,25 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+  text: "First test to do"
+},
+{
+  text: "Second test to do"
+}];
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done())
+  Todo.remove({}).then(() => {
+  return Todo.insertMany(todos);
+}).then(() => done());
 });
 
-describe('POST / todo', () => {
+describe('POST / todos', () => {
   it("should create a new todo", (done) => {
     var text = "Test todo text";
 
 request(app)
-.post('/todo')
+.post('/todos')
 .send({text})
 .expect(200)
 .expect((res) => {
@@ -24,7 +33,7 @@ request(app)
     return done(err);
   }
 
-  Todo.find().then((todos) => {
+  Todo.find({text}).then((todos) => {
     expect(todos.length).toBe(1);
     expect(todos[0].text).toBe(text);
     done();
@@ -35,7 +44,7 @@ request(app)
 
 it("should not create todo with invalid data", (done) => {
   request(app)
-  .post('/todo')
+  .post('/todos')
   .send({})
   .expect(400)
   .end((err, res) => {
@@ -44,10 +53,22 @@ it("should not create todo with invalid data", (done) => {
     }
 
 Todo.find().then((todos) => {
-  expect(todos.length).toBe(0);
+  expect(todos.length).toBe(2);
   done();
 }).catch((e) => done(e))
 
-  });
 });
+});
+});
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+    .get('/todos')
+    .expect(200)
+    .expect((res) => {
+    expect(res.body.todos.length).toBe(2)
+    })
+    .end(done);
+  });
 });
