0f14dd19a289123906373f8dd5957d45bd48bf6a
David Amazing <David Amazing>
2017-05-28 20:30:55 -0400

    hashed passwords

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-}
+var password = '123abc!';
 
-var token = jwt.sign(data, "123abc");
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+// bcrypt.hash(password, salt, (err, hash) => {
+//   console.log(hash);
+// });
+// });
 
-var decoded = jwt.verify(token, "123abc");
-console.log("Decoded", decoded);
+
+var hashP = "$2a$10$MrFDoM6RvV7oQDE5AFpkHufEz6xfvFvRe6B4Kh6ibdz4pNRls3ecO";
+bcrypt.compare(password, hashP, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id: 10
+// }
+//
+// var token = jwt.sign(data, "123abc");
+// console.log(token);
+//
+// var decoded = jwt.verify(token, "123abc");
+// console.log("Decoded", decoded);
 
 // var message = 'This is the example';
 // var hash = SHA256(message).toString();
